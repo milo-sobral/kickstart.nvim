f59f9378cd3ec49642beb90c3b7747e0114c54f0
Milo Sobral <milosobral@gmail.com>
2024-10-15 17:25:42 -0400

    added transparency

--- init.lua
+++ init.lua
@@ -831,6 +831,10 @@ require('lazy').setup({
     'catppuccin/nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
+      --- Set transparent background option
+      require('catppuccin').setup {
+        transparent_background = true,
+      }
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
